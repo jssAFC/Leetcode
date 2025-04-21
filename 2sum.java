7c069555f10c4d7878e81416af3534e355dd5932
Jaideep singh sekhon <jss3392192@gmail.com>
2025-04-22 02:00:12 +0530

    2 sum

--- 2sum.java
+++ 2sum.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int i=0;
-        int j=numbers.length-1;
-        while(i<=j){
-            int sum=numbers[i]+numbers[j];
-            if(sum==target) return new int []{i+1,j+1};
-            if (sum > target) j--;
-else i++;
+    public int[] twoSum(int[] nums, int target) {
+        Map <Integer,Integer> map= new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int diff=target-nums[i];
+            if(map.containsKey(diff)) {
+                return new int[]{map.get(diff),i};
+            }
+            map.put(nums[i],i);
         }
 
         return null;
