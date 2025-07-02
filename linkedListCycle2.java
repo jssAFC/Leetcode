dd4764d280bb27a1f4f84d5ed06681e16856a007
Jaideep singh sekhon <jss3392192@gmail.com>
2025-07-02 09:32:18 +0530

    Linked List Cycle 2

---
+++ linkedListCycle2.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow) break;
+        }
+
+        if(fast==null || fast.next==null) return null;
+        slow=head;
+        while(fast!=slow){
+            fast=fast.next;
+            slow=slow.next;
+        }
+
+        return slow;
+    }
+}
