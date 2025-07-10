ec19c48eea74ed50bfbb997f4862ee95413aa355
Jaideep singh sekhon <jss3392192@gmail.com>
2025-07-10 10:49:57 +0530

    Maximum Element After Decreasing and Rearranging

---
+++ maximumElementAfterDecrementingAndRearranging.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        if(arr[0]!=1) arr[0]=1;
+        for(int i=0;i<arr.length-1;i++){
+            if(Math.abs(arr[i+1]-arr[i])>1){
+                arr[i+1]=arr[i]+1;
+            }
+        }
+
+        return arr[arr.length-1];
+    }
+}
