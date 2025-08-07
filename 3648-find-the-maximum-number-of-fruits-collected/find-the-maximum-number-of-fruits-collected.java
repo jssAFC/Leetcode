050984c89c5e529bf3da94be7a085a0507b65dfa
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2025-08-08 02:32:56 +0530

    Time: 17 ms (52.38%) | Memory: 198 MB (42.86%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.java
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.java
@@ -1,74 +1,44 @@
 class Solution {
-    int n;
-    int t[][];
+    public int maxCollectedFruits(int[][] fruits) {
+        int n = fruits.length;
+        int[][] t = new int[n][n];
 
-    public int child1(int[][] fruits) {
-        int sum = 0;
-        for (int i = 0; i < fruits.length; i++) {
-            sum += fruits[i][i];
-            t[i][i] = 0;
+        // child1Collect - Diagonal elements
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += fruits[i][i];
         }
 
-        return sum;
-    }
+        // nullifying the cells child2 and child3 cannot visit     
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i + j < n - 1)
+                    t[i][j] = 0;
+                else
+                    t[i][j] = fruits[i][j];
+            }
+        }
 
-    public int child2(int i, int j, int[][] fruits) {
-        if (i >= n || j >= n || i < 0 || j < 0)
-            return 0;
+        // child2 collect fruits
 
-        if (i >= j)
-            return 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                t[i][j] += Math.max(t[i - 1][j - 1], Math.max(t[i - 1][j], (j + 1 < n) ? t[i - 1][j + 1] : 0));
+            }
+        }
 
-        if (i == n - 1 && j == n - 1)
-            return 0;
+        // child3 collexts
 
-        if (t[i][j] != -1)
-            return t[i][j];
+        for (int j = 1; j < n; j++) {
+            for (int i = j + 1; i < n; i++) {
+                t[i][j] += Math.max(t[i - 1][j - 1], Math.max(t[i][j - 1], (i + 1 < n) ? t[i + 1][j - 1] : 0));
+            }
+        }
 
-        int leftDiagonal = fruits[i][j]+child2(i + 1, j - 1, fruits);
-        int rightDiagonal =fruits[i][j]+ child2(i + 1, j + 1, fruits);
-        int down = fruits[i][j]+child2(i + 1, j, fruits);
+        System.out.println(t[n - 1][n - 2]);
+        System.out.println(t[n - 2][n - 1]);
 
-        return t[i][j]=Math.max(leftDiagonal, Math.max(down, rightDiagonal));
-    }
-
-    public int child3(int i, int j, int[][] fruits) {
-        if (i >= n || j >= n || i < 0 || j < 0)
-            return 0;
-
-        if (j >= i)
-            return 0;
-
-        if (i == n - 1 && j == n - 1)
-            return 0;
-
-        if (t[i][j] != -1)
-            return t[i][j];
-
-        int topDiagonal =fruits[i][j]+ child3(i - 1, j + 1, fruits);
-        int rightDiagonal =fruits[i][j]+ child3(i + 1, j + 1, fruits);
-        int right =fruits[i][j]+ child3(i, j+1, fruits);
-
-        return t[i][j]=Math.max(topDiagonal, Math.max(right, rightDiagonal));
-    }
-
-    public int maxCollectedFruits(int[][] fruits) {
-        n = fruits.length;
-        t = new int[n][n];
-
-        for (int[] i : t)
-            Arrays.fill(i, -1);
-
-        int firstChildScore = child1(fruits);
-
-        int secondChildScore = child2(0, n - 1, fruits);
-
-        int thirdChildScore = child3(n - 1, 0, fruits);
-
-        System.out.println(firstChildScore);
-        System.out.println(secondChildScore);
-        System.out.println(thirdChildScore);
-
-        return firstChildScore + secondChildScore + thirdChildScore;
+        
+        return result + t[n - 2][n - 1] + t[n - 1][n - 2];
     }
 }
