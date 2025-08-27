479ebfa77d31ac5a8c24566c538aa36e29eb4eb9
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2025-08-27 13:31:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (17.26%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -1,24 +1,21 @@
 class Solution {
+    private long m = (long) Math.pow(10, 9) + 7;
+    
     public long power(long n, long p) {
-        long m = (long) Math.pow(10, 9) + 7;
-        n %= m;
-        long result = 1;
-        while (p > 0) {
-            if ((p & 1) == 1) {
-                result = (result * n) % m;
-            }
-
-            n = (n * n) % m;
-            p >>= 1;
+        if(p==0) return 1;
+        long result=power(n,p/2);
+        if(p%2==0) {
+            return (result*result) % m;
         }
 
-        return result;
+        return (result*result*n)%m;
+
+
     }
 
     public int countGoodNumbers(long n) {
-        long m = (long) Math.pow(10, 9) + 7;
+        
 
-        n = n;
         long even = ((n + 1) / 2) ;
         long odd = (n / 2) ;
 
