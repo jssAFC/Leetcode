824362b1b0a8a611385be033b92815cb0a4c09e2
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2025-09-06 21:12:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (76.56%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,51 +1,26 @@
 class Solution {
-    boolean checkValid(String sb) {
-        Stack<Character> st = new Stack<>();
-        for (int i = 0; i < sb.length(); i++) {
-            char ch = sb.charAt(i);
-            if (ch == '(')
-                st.push('(');
-            else {
-                if (!st.isEmpty() && st.peek() == '(')
-                    st.pop();
-                else
-                    return false;
-            }
-        }
-
-        return st.isEmpty() ? true : false;
-    }
-
-    void solve(StringBuilder sb, int len, List<String> list,int open,int close) {
+    public void solve(List<String> result, StringBuilder sb, int open, int close, int len) {
+        if (open > len / 2 || close > open)
+            return;
         if (sb.length() == len) {
-            // if (checkValid(sb.toString()) == true)
-            if(open==close)
-            list.add(sb.toString());
+            result.add(sb.toString());
             return;
         }
 
-        if (open < close || open>len)
-            return;
+        solve(result, sb.append("("), open + 1, close, len);
+        sb.setLength(sb.length() - 1);
+        solve(result, sb.append(")"), open, close + 1, len);
+        sb.setLength(sb.length() - 1);
 
-        solve(sb.append("("), len, list, open + 1, close);
-        sb.setLength(sb.length() - 1);
-        solve(sb.append(")"), len, list, open, close + 1);
-        sb.setLength(sb.length() - 1);
     }
 
     public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<>();
-        List<String> result = new ArrayList<>();
+        int open = 0, close = 0;
+        List<String> result = new ArrayList();
         StringBuilder sb = new StringBuilder();
-        int open = 0;
-        int close = 0;
-        solve(sb, 2 * n, list, open, close);
 
-        // for (String s : list) {
-        //     if (checkValid(s))
-        //         result.add(s);
-        // }
-        // return result;
-        return list;
+        solve(result, sb, open, close, 2 * n);
+
+        return result;
     }
 }
