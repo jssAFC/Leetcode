cb543313b5cd80c054a9d7517ae6d43c63b29647
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2025-10-20 23:44:24 +0530

    Time: 1 ms (74.53%) | Memory: 43 MB (34.33%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int init_val=0;
+
+        for(String s:operations){
+            if(s.contains("++")) init_val++;
+            else init_val--;
+        }
+
+        return init_val;
+    }
+}
