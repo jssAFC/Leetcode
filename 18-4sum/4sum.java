831be7dd82e08f781d2ad06fc8d50ea12b7bfd28
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2025-10-29 00:04:32 +0530

    Time: 18 ms (63.91%) | Memory: 44.2 MB (33.76%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,36 +1,57 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Set<List<Integer>> result=new HashSet<>();
+        // Use a List directly instead of a Set for better performance
+        List<List<Integer>> result = new ArrayList<>();
+        
+        if (nums == null || nums.length < 4) {
+            return result;
+        }
 
         Arrays.sort(nums);
+        int n = nums.length;
 
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                int start=j+1,end=nums.length-1;
+        for (int i = 0; i < n - 3; i++) {
+            // SKIP DUPLICATES for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-                while(start<end){
-                long sum=(long)nums[i]+nums[j]+nums[start]+nums[end];
-                if(result.contains(sum)) continue;
-                // System.out.println(sum);
-                    if(sum==target){
-                        result.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[start],nums[end])));
+            for (int j = i + 1; j < n - 2; j++) {
+                // SKIP DUPLICATES for j
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue; 
+                }
+
+                int start = j + 1;
+                int end = n - 1;
+
+                while (start < end) {
+                    // FIX 1: Use (long) to prevent overflow
+                    long sum = (long)nums[i] + nums[j] + nums[start] + nums[end];
+
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[start], nums[end]));
+                        
+                        // FIX 2: Skip duplicates for start and end
+                        while (start < end && nums[start] == nums[start + 1]) {
+                            start++;
+                        }
+                        while (start < end && nums[end] == nums[end - 1]) {
+                            end--;
+                        }
+                        
+                        // Move both pointers
                         start++;
-                    }
-
-                    else if(sum>target){
                         end--;
-                    }
-
-                    else{
+                        
+                    } else if (sum > target) {
+                        end--;
+                    } else { // sum < target
                         start++;
                     }
-
                 }
             }
         }
-
-        return new ArrayList<>(result);
+        return result;
     }
-
-    
 }
