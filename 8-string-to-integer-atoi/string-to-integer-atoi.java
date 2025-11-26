bee4d7e3bcebc99131d1055208d25370188fb91c
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2025-11-26 23:24:54 +0530

    Time: 1 ms (99.99%) | Memory: 43.3 MB (35.85%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,44 +1,33 @@
 class Solution {
-    private long result=0;
-    boolean flag=false;
-
-    public void solve(int i,String s,int n){
-        if(i>=n || s.charAt(i)<'0' || s.charAt(i)>'9') return;
-        int digit=s.charAt(i)-'0';
-
-        if((Integer.MAX_VALUE / 10 < result || Integer.MAX_VALUE / 10 == result && Integer.MAX_VALUE % 10 < digit)){
-            flag=true;
-            return;
-        }
-        result=result*10+digit;
-
-        System.out.println(result);
-
-        solve(i+1,s,n);
-
-    }
     public int myAtoi(String s) {
-        int i = 0, n = s.length();
-        // if(n==0) return 0;
-        int num = 0;
-        while (i<n && s.charAt(i) == ' ')
+        int i = 0;
+        boolean positive = true;
+        long num = 0;
+        while (i < s.length() && s.charAt(i) == ' ')
             i++;
-        if (i == n)
-            return num;
-
-        int sign = 1;
-
-        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-            sign = s.charAt(i) == '+' ? 1 : -1;
+        if (i < s.length() && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
+            if (s.charAt(i) == '-')
+                positive = false;
             i++;
         }
 
-        solve(i,s,n);
+        while (i < s.length() && s.charAt(i) == '0')
+            i++;
 
-        if(flag==true){
-            return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;
+        if (i < s.length() && !Character.isDigit(s.charAt(i)))
+            return 0;
+
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            int digit=s.charAt(i)-'0';
+
+            if(positive && (Integer.MAX_VALUE - digit)/10<num) return Integer.MAX_VALUE;
+            else if((Integer.MIN_VALUE + digit)/10>-num) return Integer.MIN_VALUE;
+            num = num * 10 + (digit);
+            i++;
         }
-        return sign * (int)result;
 
+        
+
+        return positive?(int) num:(int)-num;
     }
 }
