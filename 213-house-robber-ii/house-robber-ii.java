347af2048a4f902d8999d3e5970564a2d9502983
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2025-12-10 16:47:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (57.29%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,27 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-        int[] dp1=new int[nums.length-1];
-        int[] dp2=new int[nums.length-1];
         
         int[] arr1=Arrays.copyOfRange(nums,0,n-1);
-        // dp1[0]=arr1[0];
         int[] arr2=Arrays.copyOfRange(nums,1,n);
-        // dp2[0]=arr2[0];
 
-        return Math.max(solve(arr1,dp1),Math.max(solve(arr2,dp2),nums[0]));
+        return Math.max(solve(arr1),Math.max(solve(arr2),nums[0]));
     }
 
-    public int solve(int[] arr,int[] dp){
+    public int solve(int[] arr){
         if(arr.length==0) return 0;
-        dp[0]=arr[0];
-        for(int i=1;i<arr.length;i++){
-            int fs=dp[i-1];
-            int ss=arr[i];
-            if(i>1) ss+=dp[i-2];
-            dp[i]=Math.max(fs,ss);
+        int prev1=0,prev2=0;
+        for(int i=0;i<arr.length;i++){
+            int curr=arr[i]+prev2;
+
+            curr=Math.max(curr,prev1);
+
+            prev2=prev1;
+            prev1=curr;
         }
 
-        return dp[dp.length-1];
+        return prev1;
+
+        // return dp[dp.length-1];
     }
 }
