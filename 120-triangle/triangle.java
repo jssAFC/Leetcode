67b5d197bd18ebf8405287c0b1a5d8f376890c8a
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2025-12-24 16:49:36 +0530

    Time: 14 ms (5.42%) | Memory: 46.8 MB (7.25%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,26 +1,31 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         List<List<Integer>> dp = new ArrayList<>();
-        for (int i = 0; i < triangle.size(); i++) {
+        int m=triangle.size();
+        for (int i = 0; i < m; i++) {
             List<Integer> list = new ArrayList<>();
             for (int j = 0; j < triangle.get(i).size(); j++) {
                 list.add(null);
             }
             dp.add(list);
         }
-        return solve(triangle,dp, 0, 0);
+
+        for(int i=m-1;i>=0;i--){
+            for(int j=0;j<triangle.get(i).size();j++){
+                int val=triangle.get(i).get(j);
+                if(i==m-1){
+                    dp.get(i).set(j,val);
+                } else{
+                    int betterVal=triangle.get(i).get(j)+Math.min(dp.get(i+1).get(j),dp.get(i+1).get(j+1));
+                    dp.get(i).set(j,betterVal);
+                }
+            }
+        }
+
+        
+        return dp.get(0).get(0);
 
     }
 
-    public int solve(List<List<Integer>> arr, List<List<Integer>> dp, int i, int j) {
-        if (i >= arr.size())
-            return 0;
-
-        if (dp.get(i).get(j) != null)
-            return dp.get(i).get(j);
-
-        int res=arr.get(i).get(j) + Math.min(solve(arr, dp, i + 1, j), solve(arr, dp, i + 1, j + 1));
-        dp.get(i).set(j,res);
-        return res;
-    }
+    
 }
