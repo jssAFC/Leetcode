042cdf849294613df28553da5532754d04a5d5f9
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2026-01-02 21:03:07 +0530

    Time: 6 ms (67.41%) | Memory: 47.1 MB (64.76%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,43 +1,31 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n=matrix.length;
-        int[][] dp = new int[n][n];
-        for(int i=0;i<n;i++)
-            dp[0][i]=matrix[0][i];
+        int[] prev=new int[n];
+        int[] curr=new int[n];
 
-            
+        for(int i=0;i<n;i++)
+            prev[i]=matrix[0][i];
+
+
         for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
-                int fs=(j-1<0)?Integer.MAX_VALUE:dp[i-1][j-1];
-                int ss=dp[i-1][j];
-                int ts=(j+1>=n)?Integer.MAX_VALUE:dp[i-1][j+1];
+                int fs=(j-1<0)?Integer.MAX_VALUE:prev[j-1];
+                int ss=prev[j];
+                int ts=(j+1>=n)?Integer.MAX_VALUE:prev[j+1];
 
-                dp[i][j]=matrix[i][j]+Math.min(fs,Math.min(ss,ts));
-
-                System.out.println(dp[i][j]);
+                curr[j]=matrix[i][j]+Math.min(fs,Math.min(ss,ts));
             }
-        }
+prev = curr.clone();        }
 
-        int min=dp[n-1][0];
+        int min=prev[0];
 
         for(int i=0;i<n;i++)
-            min=Math.min(dp[n-1][i],min);
+            min=Math.min(prev[i],min);
         
 
         return min;
     }
 
-    public int solve(int i, int j, int[][] mat, int[][] dp, int n) {
-        if (j >= n || j < 0)
-            return Integer.MAX_VALUE;
 
-        if (i == n - 1)
-            return dp[i][j] = mat[i][j];
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        return dp[i][j] = mat[i][j] + Math.min(solve(i + 1, j - 1, mat, dp, n),
-                Math.min(solve(i + 1, j, mat, dp, n), solve(i + 1, j + 1, mat, dp, n)));
-    }
 }
