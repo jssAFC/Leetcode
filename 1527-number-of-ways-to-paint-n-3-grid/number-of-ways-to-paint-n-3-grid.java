96248a88e0c33a96738b0f623f75a2741688f6ee
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2026-01-06 00:41:54 +0530

    Time: 3 ms (95.06%) | Memory: 42.1 MB (55.54%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.java
@@ -1,64 +1,16 @@
 class Solution {
-    List<String> rowSequence = new ArrayList<>();
-    public static final int M = 1000000007;
-    int[][]dp;
-
-    public void generateRowSequence(String str, String prev, int i) {
-        if (i == 3) {
-            rowSequence.add(str);
-            return;
-        }
-
-        String[] colors = { "R", "G", "Y" };
-        for (String s : colors) {
-            if (s.equals(prev))
-                continue;
-            generateRowSequence(str + s, s, i + 1);
-        }
-    }
-
-    public int solve(int remRows, int prevIndex) {
-        if (remRows == 0)
-            return 1;
-
-        if(dp[remRows][prevIndex]!=-1) return dp[remRows][prevIndex];
-        int ways = 0;
-        String prevState = rowSequence.get(prevIndex);
-
-        for (int i = 0; i < rowSequence.size(); i++) {
-            if (i == prevIndex)
-                continue;
-
-            String currState = rowSequence.get(i);
-            boolean valid = true;
-            // System.out.println(currState);
-            for (int j = 0; j < 3; j++) {
-                if (prevState.charAt(j) == currState.charAt(j)) {
-                    valid = false;
-                    break;
-                }
-            }
-
-            if (valid) {
-                ways = (ways + solve(remRows - 1, i)) % M;
-            }
-
-        }
-        return dp[remRows][prevIndex]=ways;
-    }
+    static final int M=1_000_000_007;
+    long colors2=6;
+    long colors3=6;
 
     public int numOfWays(int n) {
-        generateRowSequence("", "#", 0);
-        dp=new int[n][rowSequence.size()+1];
-
-        for(int []arr:dp) Arrays.fill(arr,-1);
-        
-        int result = 0;
-
-        for (int i = 0; i < rowSequence.size(); i++) {
-            result = (result + solve(n - 1, i)) % M;
+        for(int i=1;i<n;i++){
+            long nextColor2= 3*colors2 + 2*colors3;
+            long nextColor3= 2*colors2 + 2*colors3;
+            colors2=nextColor2%M;
+            colors3=nextColor3%M;
         }
 
-        return result;
+        return (int)(colors2+colors3)%M;
     }
 }
