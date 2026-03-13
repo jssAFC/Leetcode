3324e097ea1ed43636bf4f807bc26dc25c78b147
Jaideep Singh Sekhon <144608445+jssAFC@users.noreply.github.com>
2026-03-14 01:03:02 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (93.47%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -7,26 +7,19 @@ class Solution {
         Arrays.fill(colors, -1);
         int color = 0;
 
-        for (int i = 0; i < V; i++) {
-            if (colors[i] == -1 && !isCycle(graph, i, -1, color, colors)) {
-                return false;
-            }
+        for(int i=0;i<V;i++){
+            if(colors[i]==-1 && dfs(graph,colors,i,-1,1-color)==false) return false;
         }
 
         return true;
     }
 
-    public boolean isCycle(int[][] graph, int i, int parent, int color, int[] colors) {
-        colors[i]=color;
-        for (int u : graph[i]) {
-            if (colors[u] == -1) {
-                if (isCycle(graph, u, i, 1 - color, colors) == false)
-                    return false;
-            } else {
-                if (colors[u] == color)
-                    return false;
-            }
+    public boolean dfs(int[][] graph,int[] colors,int u, int parent,int color){
+        colors[u]=color;
 
+        for(int i:graph[u]){
+            if(colors[i]==-1 && dfs(graph,colors,i,u,1-color)==false) return false;
+            else if(i!=parent && colors[i]==colors[u]) return false;
         }
 
         return true;
