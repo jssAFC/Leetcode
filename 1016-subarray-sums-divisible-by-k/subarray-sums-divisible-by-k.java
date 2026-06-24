b94ada4e077a07043be5798c6326009f62c1ab87
Jaideep Singh Sekhon <144608445+jaideepSekhon@users.noreply.github.com>
2026-06-25 01:18:07 +0530

    Time: 25 ms (54.14%) | Memory: 51.7 MB (35.11%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -5,13 +5,14 @@ class Solution {
         map.put(0,1);
 
         for(int i=0;i<nums.length;i++){
-            presum=presum+nums[i];
+            presum+=nums[i];
             if(presum<0) presum=(presum%k)+k;
+
             if(map.containsKey(presum%k)){
                 count+=map.get(presum%k);
             }
 
-            map.put(presum%k,map.getOrDefault(presum%k ,0)+1);
+            map.put(presum%k,map.getOrDefault(presum%k,0)+1);
         }
 
         return count;
