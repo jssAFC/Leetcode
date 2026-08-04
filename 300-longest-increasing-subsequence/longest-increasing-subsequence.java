eaf5a5e8e79a7335b543ca66759560afb6d5e396
Jaideep Singh Sekhon <144608445+jaideepSekhon@users.noreply.github.com>
2026-08-04 15:37:39 +0530

    Time: 398 ms (8.02%) | Memory: 237.3 MB (7.58%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,18 +1,29 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int max=0,n=nums.length;
-        int[] dp=new int[n];
-        Arrays.fill(dp,1);
+    int[][] dp;
 
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]) dp[i]=Math.max(dp[j]+1,dp[i]);
-            }
+    public int lengthOfLIS(int[] nums) {
+        int max=0;
+        dp=new int[nums.length][nums.length+1];
+        
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
         }
 
-        for(int num:dp) max=Math.max(num,max);
+        // for(int i=0;i<nums.length;i++){
+            max=Math.max(max,func(0,-1,nums));
+        // }
 
         return max;
-        
+    }
+
+    public int func(int i,int prev,int[] nums){
+        if(i>=nums.length) return 0;
+
+        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+
+        int take=0;
+        if(prev==-1 || nums[i]>nums[prev]) take= 1+func(i+1,i,nums);
+
+        return dp[i][prev+1]=Math.max(take,func(i+1,prev,nums));
     }
 }
